ac8c3089d72c4f180cc7080408ede13fbce78964
Suman <sumanrawool2@gmail.com>
2023-03-10 18:18:42 +0530

    created serializer for the user

--- quickstart/app/serializers.py
+++ quickstart/app/serializers.py
@@ -6,3 +6,11 @@ class EmployeeSerializer(serializers.Serializer):
     email=serializers.EmailField()
     password=serializers.CharField(max_length=30)
     phone=serializers.CharField(max_length=10)
+
+class UserSerializer(serializers.Serializer):
+    username=serializers.CharField(max_length=30)
+    email=serializers.EmailField()
+    password=serializers.CharField(max_length=30)
+    first_name=serializers.CharField(max_length=30)
+    last_name=serializers.CharField(max_length=30)
+
--- quickstart/app/views.py
+++ quickstart/app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Employee
-from .serializers import EmployeeSerializer
+from .serializers import EmployeeSerializer,UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
 def employeeListView(request):
     employees=Employee.objects.all()
     serializer=EmployeeSerializer(employees,many=True)
+    return JsonResponse(serializer.data,safe=False)
+
+def userListView(request):
+    users=User.objects.all()
+    serializer=UserSerializer(users,many=True)
     return JsonResponse(serializer.data,safe=False)
